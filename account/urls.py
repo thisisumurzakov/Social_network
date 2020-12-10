3c185f05f2eb486048092ae77bf18ed03474359c
thisisumurzakov <thisisumurzakov@gmail.com>
2020-12-11 02:17:36 +0500

    user regisration

--- account/urls.py
+++ account/urls.py
@@ -6,6 +6,9 @@ from . import views
 urlpatterns = [
     #path('login/', views.user_login, name='login'),
     path('', views.dashboard, name='dashboard'),
+
+    path('register/', views.register, name='register'),
+
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
--- account/views.py
+++ account/views.py
@@ -3,13 +3,26 @@ from django.contrib.auth import login, authenticate
 from django.http import HttpResponse
 from  django.contrib.auth.decorators import login_required
 
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
 
 @login_required()
 def dashboard(requst):
     return render(requst, 'account/main.html', {'section': 'dashboard'})
 
-def user_login(request):
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password'])
+            # Сохраняем пользователя в базе данных.
+            new_user.save()
+            return render(request, 'account/register_done.html', {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, 'account/register.html', {'user_form': user_form})
+
+"""def user_login(request):
     form = LoginForm()
     if request.method == 'POST':
         form = LoginForm(request.POST)
@@ -24,4 +37,4 @@ def user_login(request):
                 return HttpResponse('Disabled account')
         else:
             return HttpResponse('Invalid login')
-    return render(request, 'account/login.html', {'form': form})
+    return render(request, 'account/login.html', {'form': form})"""
