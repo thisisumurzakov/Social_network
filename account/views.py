7b0756230969312dced12407fa9d398536af7d82
thisisumurzakov <thisisumurzakov@gmail.com>
2020-12-16 00:23:04 +0500

    notifications

--- account/views.py
+++ account/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.shortcuts import render
 from django.contrib.auth import login, authenticate
 from django.http import HttpResponse
@@ -32,6 +33,9 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request, 'Profile updated successfully')
+        else:
+            messages.error(request, 'Error updating your profile')
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
